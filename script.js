aa4a6d7396b418d4063abfd6eb9d65996913345e
Anuj <anujgupta4155@gmail.com>
2024-02-15 17:08:16 +0530

    designing voting section

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 
-document.getElementsByClassName('container-md')[0].addEventListener('click', function() {
-  console.log('hello')
-  document.getElementById('file').click();
-});
 
-document.getElementById('file').addEventListener('change', function() {
-  // Handle the file selection here
-  var selectedFile = this.files[0];
-  console.log("Selected file:", selectedFile);
-});
+function test () {
+  console.log("verified")
+}
