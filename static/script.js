d53b4e3ff5da9bc95a01edce3184f98a2b4f0468
Anuj <anujgupta4155@gmail.com>
2024-07-10 11:27:10 +0530

    adding update package.json file and some extra feature

--- static/script.js
+++ static/script.js
@@ -1,27 +1,27 @@
-const urlofimg = document.getElementById('urlofimg');
-const title = document.getElementById('title');
-const img1 = document.getElementById('img1');
-const rankDisplayer = document.getElementById('rankDisplayer')
+const urlofimg = document.getElementById("urlofimg");
+const title = document.getElementById("title");
+const img1 = document.getElementById("img1");
+const rankDisplayer = document.getElementById("rankDisplayer");
 
-function getDataFromDb(){
-fetch('http://127.0.0.1:5000/get-images')
-.then(response => response.json())
-.then(data => onload(showImage(data)))}
+function getDataFromDb() {
+  fetch("http://127.0.0.1:5000/get-images")
+    .then((response) => response.json())
+    .then((data) => onload(showImage(data)));
+}
 // setInterval(showImage, 5000)}
 
-window.onload = function(){
-  getDataFromDb()
-  showCampaireImages()
-}
-function showImage(data ){
-  for (let i= 0; i <= data.length ; i++  ){  
-  value = data.length
-  let image = document.createElement('img')
-  image.setAttribute('src' ,data[i][1])
-  image.setAttribute('width' , '200')
-  image.setAttribute('class', 'responsive-img')
-  rankDisplayer.appendChild(image)
-
+window.onload = function () {
+  getDataFromDb();
+  showCampaireImages();
+};
+function showImage(data) {
+  for (let i = 0; i <= data.length; i++) {
+    value = data.length;
+    let image = document.createElement("img");
+    image.setAttribute("src", data[i][1]);
+    image.setAttribute("width", "200");
+    image.setAttribute("class", "responsive-img");
+    rankDisplayer.appendChild(image);
   }
 }
 
@@ -31,15 +31,15 @@ const heading1 = document.getElementById("heading-1");
 const heading2 = document.getElementById("heading-2");
 
 async function showCampaireImages() {
-  const response = await fetch('http://127.0.0.1:5000/get-compare-images');
+  const response = await fetch("http://127.0.0.1:5000/get-compare-images");
   const data = await response.json();
-  console.log(data)
-  image1.setAttribute("src", data[0][2])
-  image2.setAttribute("src", data[1][2])
-  image1.onclick = () => createAndSubmitForm(data[0][0], data[1][0])
-  image2.onclick = () => createAndSubmitForm(data[1][0], data[0][0])
-  heading1.innerText = data[0][1]
-  heading2.innerText = data[1][1]
+  console.log(data);
+  image1.setAttribute("src", data[0][2]);
+  image2.setAttribute("src", data[1][2]);
+  image1.onclick = () => createAndSubmitForm(data[0][0], data[1][0]);
+  image2.onclick = () => createAndSubmitForm(data[1][0], data[0][0]);
+  heading1.innerText = data[0][1];
+  heading2.innerText = data[1][1];
 }
 
 // async function handleOnClick(selectedId, remainingId) {
@@ -60,27 +60,28 @@ async function showCampaireImages() {
 //   console.log(res.status);
 
 // }
+
 function createAndSubmitForm(selectedID, remainingID) {
   // Create a new FormData object
   var formData = new FormData();
 
   // Add data to the FormData object
-  formData.append('selectedID', selectedID); // Example value
-  formData.append('remainingID', remainingID); // Example value
+  formData.append("selectedID", selectedID); // Example value
+  formData.append("remainingID", remainingID); // Example value
 
   // Create a new XMLHttpRequest object
   var xhr = new XMLHttpRequest();
 
   // Open a POST request to the server endpoint
-  xhr.open('POST', '/post-image-vote');
+  xhr.open("POST", "/post-image-vote");
 
   // Set up a callback function to handle the response
-  xhr.onload = function() {
-      if (xhr.status === 200) {
-          console.log('Form submitted successfully');
-      } else {
-          console.error('Form submission failed');
-      }
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log("Form submitted successfully");
+    } else {
+      console.error("Form submission failed");
+    }
   };
 
   // Send the FormData object as the request body
